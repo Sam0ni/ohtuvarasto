e7fd20d54ca7cb3762def8ab7e3132f683a7f1f0
Samppa <samppasalo16@gmail.com>
2022-11-10 18:13:41 +0200

    add exercise 3

--- src/varasto.py
+++ src/varasto.py
@@ -1,10 +1,13 @@
+
+def inittilavuus(tilavuus: int):
+    if tilavuus > 0.0:
+        return tilavuus
+    # virheellinen, nollataan
+    return 0.0
+
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = inittilavuus(tilavuus)
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
@@ -16,7 +19,8 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
